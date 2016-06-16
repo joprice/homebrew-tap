89a1dcc662b8b0d3c5bb58f5e13c9415109d045e
Joseph Price <josephprice@iheartmedia.com>
2016-06-16 18:36:32 -0400

    add sha

--- dynamite.rb
+++ dynamite.rb
@@ -1,6 +1,6 @@
 class Dynamite < Formula
   homepage "https://github.com/joprice/dynamite"
   url "https://github.com/joprice/dynamite/releases/download/v0.0.1/dynamite-0.0.1.zip"
-  sha1 ""
+  sha256 "18f4597f6c40f6b10ef0c6a472149b491d7ee6ab1718a23c9485b1627ea57cfe"
 end
 
