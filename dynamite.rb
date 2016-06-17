8eae64f769252bca13fac3d69b8c361490879d5f
Joseph Price <josephprice@iheartmedia.com>
2016-06-16 23:37:43 -0400

    fix dynamite sha

--- dynamite.rb
+++ dynamite.rb
@@ -1,7 +1,7 @@
 class Dynamite < Formula
   homepage "https://github.com/joprice/dynamite"
   url "https://github.com/joprice/dynamite/releases/download/v0.0.4/dynamite-0.0.4.zip"
-  sha256 "9e8fa4705818845f320163c9d03b31b3de66a69252dcac639a79f6ac9f6c8cdf"
+  sha256 "8f6f1282542579f505acc6321bfd82fad5e7468b5cd8a20ffecacbec2384f7fa"
 
   def install 
     libexec.install Dir["lib"]
