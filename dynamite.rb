c6d39e544955830313be9378c5c0029af66d69fe
Joseph Price <josephprice@iheartmedia.com>
2016-06-16 23:35:38 -0400

    bump version to 0.0.4

--- dynamite.rb
+++ dynamite.rb
@@ -1,6 +1,6 @@
 class Dynamite < Formula
   homepage "https://github.com/joprice/dynamite"
-  url "https://github.com/joprice/dynamite/releases/download/v0.0.3/dynamite-0.0.3.zip"
+  url "https://github.com/joprice/dynamite/releases/download/v0.0.4/dynamite-0.0.4.zip"
   sha256 "9e8fa4705818845f320163c9d03b31b3de66a69252dcac639a79f6ac9f6c8cdf"
 
   def install 
