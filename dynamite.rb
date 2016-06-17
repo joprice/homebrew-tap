02863df2db129833e8b2e2920e474fe00b65ed9d
Joseph Price <josephprice@iheartmedia.com>
2016-06-17 15:08:41 -0400

    upgrade dynamite to 0.0.7

--- dynamite.rb
+++ dynamite.rb
@@ -1,7 +1,7 @@
 class Dynamite < Formula
   homepage "https://github.com/joprice/dynamite"
-  url "https://github.com/joprice/dynamite/releases/download/v0.0.6/dynamite-0.0.6.zip"
-  sha256 "05d3d452f08f72aee7e6e2b0e356d24e4c6e99bc2808476e49d9a083f2751562"
+  url "https://github.com/joprice/dynamite/releases/download/v0.0.7/dynamite-0.0.7.zip"
+  sha256 "b59a36a803237db60b8fd56b0365c0d9fa952a472013deb4b06ad2dc2d7faec4"
 
   def install 
     libexec.install Dir["lib"]
