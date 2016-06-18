8bda31bd94e03ed0ed10cc7bd0b3e9a4eaf23a7a
Joseph Price <josephprice@iheartmedia.com>
2016-06-18 16:43:56 -0400

    bump dynamite to 0.0.9

--- dynamite.rb
+++ dynamite.rb
@@ -1,7 +1,7 @@
 class Dynamite < Formula
   homepage "https://github.com/joprice/dynamite"
-  url "https://github.com/joprice/dynamite/releases/download/v0.0.8/dynamite-0.0.8.zip"
-  sha256 "9a86fa85d9381d648901193f608993c4b3878368f4d953294d7132f582e881b7"
+  url "https://github.com/joprice/dynamite/releases/download/v0.0.9/dynamite-0.0.9.zip"
+  sha256 "f9c130b4a85b2867728eee83617bd086658952845966a02a368721dd8ebe44a0"
 
   def install
     libexec.install "lib"
