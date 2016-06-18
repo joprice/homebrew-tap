0f4cb83d293e7a54704520a2203540fb677c4811
Joseph Price <josephprice@iheartmedia.com>
2016-06-18 12:26:36 -0400

    address brew audit issues in prs

--- prs.rb
+++ prs.rb
@@ -3,10 +3,9 @@ class Prs < Formula
   url "https://github.com/joprice/prs/releases/download/v0.0.1/prs-0.0.1.zip"
   sha256 "1a58c2d9b5fc3fa31afcf3a846485fc807a359b36397b8d95f2076df1873b43a"
 
-  def install 
-    libexec.install Dir["lib"]
-    libexec.install Dir["bin"]
+  def install
+    libexec.install "lib"
+    libexec.install "bin"
     bin.install_symlink libexec+'bin/dynamite'
   end
 end
-
