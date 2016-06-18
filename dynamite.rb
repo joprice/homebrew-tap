f95f70ca26a792cefd7b9d9a916a25ed480901ac
Joseph Price <josephprice@iheartmedia.com>
2016-06-18 12:25:56 -0400

    address brew audit issues

--- dynamite.rb
+++ dynamite.rb
@@ -3,10 +3,9 @@ class Dynamite < Formula
   url "https://github.com/joprice/dynamite/releases/download/v0.0.8/dynamite-0.0.8.zip"
   sha256 "9a86fa85d9381d648901193f608993c4b3878368f4d953294d7132f582e881b7"
 
-  def install 
-    libexec.install Dir["lib"]
-    libexec.install Dir["bin"]
+  def install
+    libexec.install "lib"
+    libexec.install "bin"
     bin.install_symlink libexec+'bin/dynamite'
   end
 end
-
