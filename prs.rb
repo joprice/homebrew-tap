0462093b5c7e3157644240ee0b4eb3636bde2e67
Joseph Price <josephprice@iheartmedia.com>
2016-08-31 11:24:10 -0400

    upgrade prs to v0.0.3

--- prs.rb
+++ prs.rb
@@ -1,7 +1,7 @@
 class Prs < Formula
   homepage "https://github.com/joprice/prs"
-  url "https://github.com/joprice/prs/releases/download/v0.0.2/prs-0.0.2.zip"
-  sha256 "d6aa31200446cb0010afa3672b22274bf893296ba48dabfac6b6311e0c0ca09c"
+  url "https://github.com/joprice/prs/releases/download/v0.0.3/prs-0.0.3.zip"
+  sha256 "d468273098d304e3610e46f2eecb66d580c979c6e9c55f21f70941a9798c9478"
 
   def install
     libexec.install "lib"
