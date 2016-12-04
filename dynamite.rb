f3696adc740eaff5c0cd68e6e4892f22e9b4c4b6
Joseph Price <josephprice@iheartmedia.com>
2016-12-03 19:49:32 -0500

    set dynamite to 0.0.13

--- dynamite.rb
+++ dynamite.rb
@@ -1,7 +1,7 @@
 class Dynamite < Formula
   homepage "https://github.com/joprice/dynamite"
-  url "https://github.com/joprice/dynamite/releases/download/v0.0.12/dynamite-0.0.12.zip"
-  sha256 "3b2bc1b7fe2a80ef54a78f1b3d1cb7ca06ac87ab7ce169825e67de1f47424fe6"
+  url "https://github.com/joprice/dynamite/releases/download/v0.0.13/dynamite-0.0.13.zip"
+  sha256 "c1780e9db35e9fd1d32c4144390f4e0944214d02d24cad8fa7c47f19859c4ba0"
 
   def install
     libexec.install "lib"
